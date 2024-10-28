c6dac404e98b0256c7d78cd43e353546d7591147
Katherine Schloe <KatherineSchloe@outlook.com>
2024-10-28 11:09:42 -0400

    styling haul update

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -2,41 +2,51 @@ import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "./services/UserService";
-import "./login.css"
+import "./login.css";
 
-export default function Login({setIsLoggedIn}) {
+export default function Login({ setIsLoggedIn }) {
   const navigate = useNavigate();
-
-  const [email, setEmail] = useState();
+  const [email, setEmail] = useState('');
 
   const loginSubmit = (e) => {
     e.preventDefault();
-    login({email})
-      .then(r =>{
-      if(r){
-      setIsLoggedIn(true)
-      navigate('/')
-      }
-      else{
-        alert("Invalid email")
-      }
-    })
+    login({ email })
+      .then(r => {
+        if (r) {
+          setIsLoggedIn(true);
+          navigate('/');
+        } else {
+          alert("Invalid email");
+        }
+      });
   };
 
   return (
-    <Form onSubmit={loginSubmit}>
-      <fieldset>
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input id="email" type="text" onChange={e => setEmail(e.target.value)} />
-        </FormGroup>
-        <FormGroup>
-          <Button>Login</Button>
-        </FormGroup>
-        <em>
-          Not registered? <Link to="/register">Register</Link>
-        </em>
-      </fieldset>
-    </Form>
+    
+    <div className="login-container">
+      <h1>CreativeU</h1>
+      <h3>the hobby app</h3>
+      <div className="login-box">
+        <Form onSubmit={loginSubmit}>
+          <fieldset>
+            <FormGroup>
+              <Label for="email">⋞Email⋟</Label>
+              <Input
+                id="email"
+                type="text"
+                className="auth-form-input"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Button className="auth-button">Login</Button>
+            </FormGroup>
+            <em className="register-link">
+              Not registered? <Link to="/register">Register</Link>
+            </em>
+          </fieldset>
+        </Form>
+      </div>
+    </div>
   );
 }
